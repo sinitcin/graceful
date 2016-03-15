2a4b9ef2a0934512eee14e046cdbd5f849f2d3d4
Zhe Wang <0x1998@gmail.com>
2016-03-15 22:35:43 +0800

    Update documentation

---
+++ search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex["graceful"] = {"doc":"Gracefully shutdown.","items":[[3,"SignalGuard","graceful","",null,null],[11,"new","","Block necessary signals (`SIGINT`, `SIGQUIT` and `SIGTERM` on *nix,\n`Ctrl+C` and `Ctrl+Break` on Windows).",0,{"inputs":[],"output":{"name":"signalguard"}}],[11,"at_exit","","Block the running thread until a signal is received. Then the\n`handler` will be called in the main thread.",0,{"inputs":[{"name":"signalguard"},{"name":"f"}],"output":null}]],"paths":[[3,"SignalGuard"]]};
+initSearch(searchIndex);
